509750f445c5264f9f5f5f2012a81c7a4e37f025
wibastidas <wibastidas77@gmail.com>
2021-05-21 15:52:53 -0300

    funcionando ok

--- src/app/guards/auto-login.guard.ts
+++ src/app/guards/auto-login.guard.ts
@@ -15,7 +15,6 @@ export class AutoLoginGuard implements CanActivate {
   ): boolean | Observable<boolean> | Promise<boolean> {
     return new Promise((resolve, reject) => {
       firebase.auth().onAuthStateChanged((user: firebase.User) => {
-        console.log('user: ', user)
         if (user) {
           this.router.navigateByUrl('/home/route');
         } else {
